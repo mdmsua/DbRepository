d301b78cde05df3c43f7f8973ee711c355c8d458
Dmytro Morozov <mdmsua@icloud.com>
2013-10-25 22:24:51 +0300

    Included 25 untracked files

---
+++ DbRepository/DbRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace DbRepository
+{
+    public class DbRepository : IDbRepository
+    {
+        private readonly Database _db;
+        static DbRepository()
+        {
+            DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory());
+        }
+
+        public DbRepository()
+        {
+            _db = DatabaseFactory.CreateDatabase();
+        }
+
+        public DbRepository(string name)
+        {
+            _db = DatabaseFactory.CreateDatabase(name);
+
+        }
+
+        public T Get<T>(string procedure, IDictionary<string, object> parameters)
+        {
+            var result = default(object);
+            var command = PrepareCommand(procedure, parameters);
+            using (var connection = _db.CreateConnection())
+            {
+                PrepareConnection(command, connection);
+                result = command.ExecuteScalar();
+            }
+            return (T)result;
+        }
+
+        private DbCommand PrepareCommand(string procedure, IDictionary<string, object> parameters)
+        {
+            var command = _db.GetStoredProcCommand(procedure);
+            if (_db.SupportsParemeterDiscovery)
+                _db.DiscoverParameters(command);
+            foreach (var parameter in parameters)
+            {
+                _db.SetParameterValue(command, _db.BuildParameterName(parameter.Key), parameter.Value);
+            }
+            return command;
+        }
+
+        private void PrepareConnection(DbCommand command, DbConnection connection)
+        {
+            if (command.Connection == default(DbConnection))
+                command.Connection = connection;
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+        }
+
+        private async Task PrepareConnectionAsync(DbCommand command, DbConnection connection)
+        {
+            if (command.Connection == default(DbConnection))
+                command.Connection = connection;
+            if (connection.State != ConnectionState.Open)
+                await connection.OpenAsync();
+        }
+
+
+        public async Task<T> GetAsync<T>(string procedure, IDictionary<string, object> parameters)
+        {
+            var result = default(object);
+            var command = PrepareCommand(procedure, parameters);
+            using (var connection = _db.CreateConnection())
+            {
+                if (_db.SupportsAsync)
+                {
+                    await PrepareConnectionAsync(command, connection);
+                    result = await command.ExecuteScalarAsync();
+                }
+                else
+                {
+                    PrepareConnection(command, connection);
+                    result = command.ExecuteScalar();
+                }
+            }
+            return (T)result;
+        }
+    }
+}
---
+++ DbRepository/IDbRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DbRepository
+{
+    public interface IDbRepository
+    {
+        T Get<T>(string procedure, IDictionary<string, object> parameters);
+        Task<T> GetAsync<T>(string procedure, IDictionary<string, object> parameters);
+    }
+}
